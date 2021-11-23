ccc30b5232372fa218906a7c546fa7358e11a39c
Anton Kantsemal <a.kantsemal@mobidev.biz>
2021-11-23 13:27:01 +0200

    jpeg compress frames

--- net.py
+++ net.py
@@ -1,14 +1,20 @@
+import io
 import pickle
 import socket
 import struct
 from multiprocessing import Queue
+
+import numpy
+from PIL import Image
 
 
 def send_frames(sock: socket.socket, frame_buffer: Queue):
     while True:
         if sock:
             frame = frame_buffer.get()
-            a = pickle.dumps(frame)
+            os = io.BytesIO()
+            Image.fromarray(frame).save(os, format="JPEG", quality=50)
+            a = pickle.dumps(os.getvalue())
             message = struct.pack("Q", len(a)) + a
             sock.sendall(message)
 
@@ -29,5 +35,6 @@ def receive_frames(sock: socket.socket, frame_buffer: Queue, payload_size: int):
                 data += sock.recv(4 * 1024)
             frame_data = data[:msg_size]
             data = data[msg_size:]
-            frame = pickle.loads(frame_data)
+            frame_jpeg = pickle.loads(frame_data)
+            frame = numpy.array(Image.open(io.BytesIO(frame_jpeg)))
             frame_buffer.put(frame)
